a789116bd5f85e3cd5de531a7cd3f304e98b9ae8
ricky <ricky_mzf@163.com>
2017-07-27 19:09:03 +0800

    version 0.1

---
+++ README.md
@@ -0,0 +1,2 @@
+Dear Teacher,
+Please review the task kindly in my Git url. Thanks in advance.
---
+++ src/pages/home/home-page/home-page.component.ts
@@ -0,0 +1,114 @@
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss']
+})
+export class HomePageComponent implements OnInit {
+  users:Array<any> = [
+    {"index":19,
+    "name":"刘雨飏",
+      "sex":"Male",
+      "name_en":"Ryane",
+      "github":"ryanemax",
+      "exam1":66,
+      "exam2":66,
+      "exam3":66},
+      {"index":7,
+      "name":"小明",
+      "sex":"Male",
+      "name_en":"Ryane",
+      "github":"ryanemax",
+      "exam1":66,
+      "exam2":66,
+      "exam3":66},
+      {"index":14,
+      "name":"Chunk",
+      "sex":"Male",
+      "name_en":"Chunk",
+      "github":"chunk",
+      "exam1":76,
+      "exam2":76,
+      "exam3":76}
+  ]
+deleteLast(){
+  this.users.pop()
+}
+saveNewUser(){
+  this.users.push({
+     "index":666,
+      "name":"New User",
+      "sex":"Male",
+      "name_en":"new_user",
+      "github":"new_user",
+      "exam1":76,
+      "exam2":76,
+      "exam3":76
+  })
+}
+  sortByAsccending(){
+    // 参考MDN Array操作的API文档 Array相关方法方法
+    this.users.sort(function (a,b) {
+      if (a.index > b.index){
+        return 1;
+      } 
+
+      if (a.index < b.index){
+        return -1;
+      }  
+      return 0;  
+    } 
+    )
+  }
+  sortByDesccending(){
+    // 参考MDN Array操作的API文档 Array相关方法
+    // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array
+    this.users.sort(function (a,b) {
+      if(a.index < b.index) {
+        return 1;
+      }
+
+      if (a.index > b.index) {
+        return -1;
+      }
+      return 0;
+    }
+    )
+  }
+  sortByRadom(){
+    // 参考MDN Array操作的API文档 Math相关方法
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+    this.users.sort(function (a,b){
+       if (Math.random() > 0.5) {
+         return 1;
+       }
+
+       if (Math.random() < 0.5) {
+         return -1;
+       }
+       return 0;
+    }
+    )
+  }
+  constructor(meta: Meta, title: Title) {
+    title.setTitle('My Home Page');
+
+    meta.addTags([ 
+      {
+        name: 'author', content: 'eddic'
+      },
+      {
+        name: 'keywords', content: 'angular 4 tutorial, angular seo'
+      },
+      {
+        name: 'description', content: 'This is my great description.'
+      },
+    ])
+  }
+
+  ngOnInit() {
+  }
+
+}
